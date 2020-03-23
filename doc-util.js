15613592cd3872e69c31f30700d69cad7ec97a05
Brandon Ruffridge <bruffridge@nasa.gov>
2020-03-23 15:14:25 -0400

    formatting changes

--- doc-util.js
+++ doc-util.js
@@ -1,40 +1,22 @@
 // dependency of release.js. from https://github.com/uswds/uswds/blob/develop/config/gulp/doc-util.js
 
-const log = require('fancy-log');
-const colors = require('ansi-colors');
-const notifier = require('node-notifier');
-const pkg = require('./package.json');
+const log = require("fancy-log");
+const colors = require("ansi-colors");
+const notifier = require("node-notifier");
+const pkg = require("./package.json");
 
-const shellPrefix = '$';
+const shellPrefix = "$";
 
 function drawFlag() {
-  log(
-    colors.white(''),
-  );
-  log(
-    colors.white('* * * * * ========================'),
-  );
-  log(
-    colors.white('* * * * * ========================'),
-  );
-  log(
-    colors.white('* * * * * ========================'),
-  );
-  log(
-    colors.white('* * * * * ========================'),
-  );
-  log(
-    colors.white('=================================='),
-  );
-  log(
-    colors.white('=================================='),
-  );
-  log(
-    colors.white('=================================='),
-  );
-  log(
-    colors.white(''),
-  );
+  log(colors.white(""));
+  log(colors.white("* * * * * ========================"));
+  log(colors.white("* * * * * ========================"));
+  log(colors.white("* * * * * ========================"));
+  log(colors.white("* * * * * ========================"));
+  log(colors.white("=================================="));
+  log(colors.white("=================================="));
+  log(colors.white("=================================="));
+  log(colors.white(""));
 }
 
 function notify(title, message, wait) {
@@ -43,62 +25,43 @@ function notify(title, message, wait) {
     message,
     icon: 'src/img/favicons/favicon-192.png',
     wait,
+    timeout: false
   });
 }
 
 module.exports = {
   pkg: {
     name: pkg.name,
-    version: pkg.version,
+    version: pkg.version
   },
 
   dirName: `${pkg.name}-${pkg.version}`,
 
   logIntroduction(message) {
-    const introMessage = message || 'NASAWDS';
-    log(
-      colors.yellow(`${introMessage} v${pkg.version}`),
-    );
+    const introMessage = message || "NASAWDS";
+    log(colors.yellow(`${introMessage} v${pkg.version}`));
     drawFlag();
   },
 
   logCommand(name, message) {
-    log(
-      shellPrefix,
-      colors.cyan(name),
-      colors.magenta(message),
-    );
+    log(shellPrefix, colors.cyan(name), colors.magenta(message));
   },
 
-
   logHelp(name, message) {
-    log(
-      shellPrefix,
-      colors.cyan(name),
-      colors.yellow(message),
-    );
+    log(shellPrefix, colors.cyan(name), colors.yellow(message));
   },
 
   logData(name, message) {
-    log(
-      colors.cyan(name),
-      colors.yellow(message),
-    );
+    log(colors.cyan(name), colors.yellow(message));
   },
 
   logError(name, message) {
-    log(
-      colors.red(name),
-      colors.yellow(message),
-    );
+    log(colors.red(name), colors.yellow(message));
     notify(`${this.dirName} gulp ${name}`, message, true);
   },
 
   logMessage(name, message) {
-    log(
-      colors.cyan(name),
-      colors.green(message),
-    );
+    log(colors.cyan(name), colors.green(message));
     notify(`${this.dirName} gulp ${name}`, message, false);
-  },
+  }
 };
--- release.js
+++ release.js
@@ -1,61 +1,78 @@
 /* from https://github.com/uswds/uswds/blob/develop/config/gulp/release.js */
 
-const del = require('del');
-const spawn = require('cross-spawn');
-const gulp = require('gulp');
-const dutil = require('./doc-util');
+const del = require("del");
+const spawn = require("cross-spawn");
+const gulp = require("gulp");
+const dutil = require("./doc-util");
 
-const task = 'release';
+const task = "release";
 
-gulp.task('make-tmp-directory', () => {
-  dutil.logMessage('make-tmp-directory', 'Creating temporary release directory.');
+gulp.task("make-tmp-directory", () => {
+  dutil.logMessage(
+    "make-tmp-directory",
+    "Creating temporary release directory."
+  );
 
-  return gulp.src('src/**/*')
-    .pipe(gulp.dest(dutil.dirName));
+  return gulp.src("src/**/*").pipe(gulp.dest(dutil.dirName));
 });
 
-gulp.task('clean-tmp-directory', () => {
-  dutil.logMessage('clean-tmp-directory', 'Deleting temporary release directory.');
+gulp.task("clean-tmp-directory", () => {
+  dutil.logMessage(
+    "clean-tmp-directory",
+    "Deleting temporary release directory."
+  );
   return del(dutil.dirName);
 });
 
-gulp.task('zip-archives', (done) => {
-  const zip = spawn('zip', [
-    '--log-info',
-    '-r',
+gulp.task("zip-archives", done => {
+  const zip = spawn("zip", [
+    "--log-info",
+    "-r",
     `./${dutil.dirName}.zip`,
     dutil.dirName,
-    '-x "*.DS_Store"',
+    '-x "*.DS_Store"'
   ]);
 
-  dutil.logMessage('zip-archives', `Creating a zip archive in ${dutil.dirName}.zip`);
+  dutil.logMessage(
+    "zip-archives",
+    `Creating a zip archive in ${dutil.dirName}.zip`
+  );
 
-  zip.stdout.on('data', (data) => {
+  zip.stdout.on("data", data => {
     if (/[\w\d]+/.test(data)) {
-      dutil.logData('zip-archives', data);
+      dutil.logData("zip-archives", data);
     }
   });
 
-  zip.stderr.on('data', (data) => {
-    dutil.logError('zip-archives', data);
+  zip.stderr.on("data", data => {
+    dutil.logError("zip-archives", data);
   });
 
-  zip.on('error', (error) => {
-    dutil.logError('zip-archives', 'Failed to create a zip archive');
+  zip.on("error", error => {
+    dutil.logError("zip-archives", "Failed to create a zip archive");
     done(error);
   });
 
-  zip.on('close', (code) => { if (code === 0) { done(); } });
+  zip.on("close", code => {
+    if (code === 0) {
+      done();
+    }
+  });
 });
 
-gulp.task(task,
+gulp.task(
+  task,
   gulp.series(
-    (done) => {
-      dutil.logMessage(task, `Creating a zip archive at ${dutil.dirName}.zip`);
+    done => {
+      dutil.logMessage(
+        task,
+        `Creating a zip archive at ${dutil.dirName}.zip`
+      );
       done();
     },
-    'init',
-    'make-tmp-directory',
-    'zip-archives',
-    'clean-tmp-directory',
-  ));
+    "init",
+    "make-tmp-directory",
+    "zip-archives",
+    "clean-tmp-directory"
+  )
+);
